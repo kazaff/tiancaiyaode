64e29151a7fd35bdc9347cadd801148afa3f77cf
kazaff <edisondik@gmail.com>
2011-09-20 00:13:00 +0800

    dbsql & dbInfo

---
+++ other/dbInfo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 版权
+ */
+
+/**
+ * 数据库统一配置信息
+ * @var array $_db
+ */
+$_db = array(
+    'host'      => 'localhost',
+    'account'	=> 'root',
+    'password'	=> 'root',
+    'port'	=> '3306',
+    'log'       => 'true',
+    'pconnect'  => 'false'
+);
